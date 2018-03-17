03cd3adf6217d119f9d8adc212bd6ef5cb00a6c0
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-17 18:39:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(sentence)
   sentence.split.collect{|word| word.reverse}.join(" ")
 end
+def reverse_each_word(sentence)
+  reverse_sentence =[]
+  sentence_array = sentence.split(" ")
+  sentence_array.collect{|word| reverse_sentence << word.reverse}
+  reverse_sentence.join(" ")
+end
